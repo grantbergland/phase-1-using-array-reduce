dc424b94a44a789bf150f821fa8847c7b1984411
grantbergland <grantbergland@gmail.com>
2022-08-03 17:41:29 -0500

    commit tree f2377dccc4ce0e820dd274ae256fac6e725f3d06

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+let sum = 0;
+
+let totalBatteries = sum
+
+for (let i = 0; i < batteryBatches.length; i++){
+    sum += batteryBatches[i];
+}
+console.log(totalBatteries);
+
+const initialValue = 0;
+totalBatteries = batteryBatches.reduce(
+    (previousValue, currentValue) => previousValue + currentValue, 
+    initialValue
+    );
+
+//     console.log(sumWithInitial)
